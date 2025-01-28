d05a09bb8e335df00e9b3de33d49313189a666b7
finaxis002 <finaxis.ai@gmail.com>
2025-01-28 13:19:13 +0530

    fdef

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -55,12 +55,12 @@ const MenuBar = () => {
                         <span>MultestepForm</span>
                     </div>
                 </li>
-                <li className={`sidebar-list-item ${getLocation('/Database')}`} onClick={() => nav('/database')}>
+                {/* <li className={`sidebar-list-item ${getLocation('/Database')}`} onClick={() => nav('/database')}>
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-shopping-bag"><path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z" /><line x1="3" y1="6" x2="21" y2="6" /><path d="M16 10a4 4 0 0 1-8 0" /></svg>
                         <span>Database</span>
                     </div>
-                </li>
+                </li> */}
             </ul>
             <div className="account-info">
                 <div className="account-info-picture">
