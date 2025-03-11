fce240b4d14c05ee649c647339ef7023cefae126
finaxis002 <finaxis.ai@gmail.com>
2025-03-11 17:30:58 +0530

    axios imported in EmployeeRegistartion

--- src/components/NewMultiStepForm/Dashboards/AdminDashboard/EmployeeRegistration.jsx
+++ src/components/NewMultiStepForm/Dashboards/AdminDashboard/EmployeeRegistration.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import axios from 'axios';
+
 
 const EmployeeRegistrationModal = ({ setShowForm }) => {
   // Initial form state with keys that match your API (update keys if needed)
