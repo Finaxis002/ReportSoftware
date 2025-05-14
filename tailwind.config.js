bba72b25613245094e327edc912489d0e47767ca
finaxis002 <finaxis.ai@gmail.com>
2025-05-14 16:22:02 +0530

    wqew

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,6 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  safelist: [
-    'accent-blue-600',
-    'accent-green-600',
-    // add any other dynamic classes you may use
-  ],
   theme: {
     extend: {},
   },
